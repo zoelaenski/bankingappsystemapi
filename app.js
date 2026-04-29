704739bdfca78ffbcef9f8e75120814ea5834bc1
zoe laenskii <abigailoluchi532@gmail.com>
2026-04-30 00:27:44 +0100

    fix app.js static files and welcome message

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
 app.use(express.json());
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to ABI Bank API' });
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/onboarding', require('./routes/onboardingRoutes'));
 app.use('/api/account', require('./routes/accountRoutes'));
 app.use('/api/transfer', require('./routes/transferRoutes'));
 app.use('/api/transaction', require('./routes/transactionRoutes'));
-
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
-
 
 module.exports = app;
